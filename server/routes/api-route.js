3e9f6cf22350c1341ad8280004e89712b778166d
INFLEXION\sreeharsha.gv <sreeharsha.gv@inflexiontech.com>
2017-08-30 18:40:11 +0530

    api_route.js, en.json, es.json updated...! meeting.css, newMeeting.css, meeting.ejs, newMeeting.ejs files added...!

--- server/routes/api-route.js
+++ server/routes/api-route.js
@@ -6,4 +6,12 @@ app.get("/",function(req, res){
   res.render("login");
 });
 
+app.get("/meeting",function(req, res){
+  res.render("meeting");
+});
+
+app.get("/newMeeting", function(req, res){
+  res.render("newMeeting");
+});
+
 module.exports = app;
