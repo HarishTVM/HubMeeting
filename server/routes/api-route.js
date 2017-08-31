64e681394c887401ac5abd5f34a629cabddc4733
naveen.keshava <naveen.k@inflexiontech.com>
2017-08-31 15:45:41 +0530

    headder issue solved

--- server/routes/api-route.js
+++ server/routes/api-route.js
@@ -6,20 +6,22 @@ var express = require('express');
 
 var app = express.Router();
 
-app.get("/",function(req, res){
+app.get("/", (req, res)=>{
   res.render("login");
 });
-
-app.get("/meeting",function(req, res){
+app.get("/meeting", (req, res)=>{
   res.render("meeting");
 });
-
-app.get("/newMeeting", function(req, res){
+app.get("/newMeeting", (req, res)=>{
   res.render("newMeeting");
 });
-
-app.get("/configure",(req, res)=>{
+app.get("/configure", (req, res)=>{
   res.render("configure");
 });
-
+app.get("/changePassword", (req, res)=>{
+  res.render("changepassword");
+});
+app.get("/activity", (req, res)=>{
+  res.render("activity");
+});
 module.exports = app;
