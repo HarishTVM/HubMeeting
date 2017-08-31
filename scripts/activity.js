f3d15c6286ee38e3d02f41d8bbb3a9c022f42a83
Haish <harish.R@inflexiontech.com>
2017-08-31 04:13:08 -0700

    I have Implemented UI design and functionality of call and member button for activity page.

--- scripts/activity.js
+++ scripts/activity.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
  $('#btn_add_hidden').hide();
  $('#btn_meeting_details').hide();
-  $('[data-toggle="tooltip"]').tooltip();   
+ $('[data-toggle="tooltip"]').tooltip();
+ var clickFlag=false;
+
  $("#btn_all").hover(function(){
    $('#btn_add').hide();
    $('#btn_add_hidden').show();
@@ -14,8 +16,26 @@ $("#btn_meeting_details_all").hover(function(){
   $('#btn_meeting_details').show();
   $('#btu_ay_notification').hide();
  },function(){
+  if(clickFlag){
   $('#btn_meeting_details').hide();
   $('#btu_ay_notification').show();
+  }
+
 });
+
+$("#btn_call_ur").click(function(){
+  $('#btn_meeting_details').show();
+  var clickFlag=true;
+ });
+
+
+ $(function() {
+    $('.div-background').hover( function(){
+       $(this).css('background-color', '#fffff0');
+    },
+    function(){
+       $(this).css('background-color', '#FFF');
+    });
+ });
 
 });
