23db281cc4eeb0e629f2fd07ca3410a57e010c1c
Haish123 <harish.r@inflexiontech.com>
2017-09-08 15:33:51 +0530

    Changed general.js...!

--- scripts/general.js
+++ scripts/general.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('#loginBtn').click(function(){
         window.location.href='/configure';
     })
-    
+
     $('#configureBtn').click(function(){
         window.location.href='/meeting';
     })
@@ -18,34 +18,16 @@ $(document).ready(function(){
     $('#meetinglist').click(function(){
         window.location.href='/meeting';
     })
- 
+
     $('#users').click(function(){
         window.location.href='/users';
     })
-    
+
 
     $('#sapacelist').click(function(){
         window.location.href='/sapacelist';
     })
 
-    $('#testbox1').contenthover({
-        effect:'slide',
-        slide_speed:300,
-        overlay_background:'#000',
-        overlay_opacity:0.8
-    });
-    $('#testbox2').contenthover({
-        effect:'slide',
-        slide_speed:300,
-        overlay_background:'#000',
-        overlay_opacity:0.8
-    });
-    $('#testbox2').contenthover({
-        effect:'slide',
-        slide_speed:300,
-        overlay_background:'#000',
-        overlay_opacity:0.8
-    });
 
 
     // $(function() {
@@ -54,7 +36,6 @@ $(document).ready(function(){
     //       $(".rd-navbar-nav li").removeClass("activelist");
     //       $(this).addClass("activelist");
     //     });
-        
+
     //   })
 });
-
