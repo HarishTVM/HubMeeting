21f262814b3a975e4590e496b5d15f1a3ba0e79f
INFLEXION\Harish <harish.r@inflexiontech.com>
2017-09-14 21:28:50 +0530

    I have changed to icon colors and text title color for space list.

--- scripts/general.js
+++ scripts/general.js
@@ -1,18 +1,50 @@
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
+    $('#loginBtn').click(function () {
+        window.location.href = '/configure';
+    });
+    $('#configureBtn').click(function () {
+        window.location.href = '/dashboard';
+    });
+    $('#meetingBtn').click(function () {
+        window.location.href = '/newmeeting';
+    });
 
-    $('#loginBtn').click(function(){
-        window.location.href='/configure';
-    })
+    $(".btn-warning1").click(function () {
 
-    $('#configureBtn').click(function(){
-        window.location.href='/dashboard';
-    })
+        if ($(this).hasClass("btn-actived")) {
+            $(this).removeClass("btn-actived");
+            $(this).children().removeClass("h-iconls-color-white");
 
-    $('#meetingBtn').click(function(){
-        window.location.href='/newmeeting';
-    })
+            if($(this).children().hasClass("linear-icon-camera-crossed")){
+                $(this).children().removeClass("linear-icon-camera-crossed");
+                $(this).children().addClass("linear-icon-camera");
+                $(this).attr("data-original-title", "Hide");
+           }
+           else if($(this).children().hasClass("linear-icon-mic-mute")){
+                $(this).children().removeClass("linear-icon-mic-mute");
+                $(this).children().addClass("linear-icon-mic");
+                $(this).attr("data-original-title", "Mute");
+           }
+        }
+        else {
+            $(this).addClass("btn-actived");
+            $(this).children().addClass("h-iconls-color-white");
+
+           if($(this).children().hasClass("linear-icon-camera")){
+                $(this).children().removeClass("linear-icon-camera");
+                $(this).children().addClass("linear-icon-camera-crossed");
+                $(this).attr("data-original-title", "Show");
+           }
+           else if($(this).children().hasClass("linear-icon-mic")){
+                $(this).children().removeClass("linear-icon-mic");
+                $(this).children().addClass("linear-icon-mic-mute");
+                $(this).attr("data-original-title", "Unmute");
+           }
+        }
+        var attr = $(this).attr
+
+    });
 
 
 
