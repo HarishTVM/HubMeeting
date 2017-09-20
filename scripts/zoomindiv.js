0ff914273226cb66960e349f0d835278c4c4a088
harish123 <harish.r@inflexiontech.com>
2017-09-20 18:52:20 +0530

    I have Updated to UI design for zoomindiv.

--- scripts/zoomindiv.js
+++ scripts/zoomindiv.js
@@ -8,43 +8,48 @@ $(document).ready(function () {
     $('.icon-info').click(function () {
         
         if (!isHovered) {
-            
-       
             $(this).parents('.icons-parents').hide();
-            $(this).parents('.parent-div').animate({
-                width: 300,
-                height: 300,
-                top: -50
-             }, 'fast');
-            $(this).closest('.principale').css({ 'z-index':'1000'});
-            $(this).parents('.icons-parents').siblings('.child-hidden').show();
-            $(this).parents('.icons-parents').siblings('.parent-close').find('.btn-close ').show();
-        
-            $(this).siblings('.btn-close ').show();
             hoverEle = $(this).parents('.parent-div');
+             $(this).parents('.parent-div').animate({
+                 width: 555,
+                 height: 300,
+                 top: -90,
+                 right:-100
+          
+             }, 'fast');
+           
+            $(this).closest('.principale').css({ 'z-index':'1000'},{'background-color':'red !important'});   
+           
+            $(this).closest('.link-image').css({'background-color':'#fff'});   
+            $(this).parents('.icons-parents').siblings('.child-hidden').show(800);
+            $(this).parents('.icons-parents').siblings('.parent-close').find('.btn-close ').show();
+            $(this).siblings('.btn-close ').show();
+        
             isHovered = true;
         }
     });
 
-    $('.btn-close ').click(function () {
+    $('.btn-close').click(function () {
        
         if (hoverEle != null) {
-        
-     
             $(hoverEle).animate({
                 height: 160,
-                width: 270,
-                top: 0    
+                width: 300,
+                top: 0,
+                left: 0  
             }, 'fast');
 
-            $(hoverEle).find('.child-hidden').hide();
-            $(this).closest('.principale').css({ 'z-index':'1'});
+           $(hoverEle).find('.child-hidden').hide();
+            $(this).closest('.principale').css({ 'z-index':'0'});
             $('.parent-div').find('.btn-close ').hide();
+            $(this).closest('.link-image').css({'background-color':'#eee'});   
             $(hoverEle).find('.icons-parents').show();
-
+            
             hoverEle = null;
             isHovered = false;
         }
     });
+
+
 
 });
