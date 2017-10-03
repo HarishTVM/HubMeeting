430bc2e6acc3078e13a58fd3cb2f07e41c51ea1a
INFLEXION\Harish123 <harish.r@inflexiontech.com>
2017-10-03 19:02:45 +0530

    I have updated layouts and ui design.

--- scripts/common/general.js
+++ scripts/common/general.js
@@ -1,9 +1,10 @@
 
 $(document).ready(function () {
-
+    var docElem = window.document.documentElement, didScroll, scrollPosition;
     // Button hover effect for meeting members
     $(".btn-warning1").click(function () {
         if ($(this).hasClass("btn-actived")) {
+            $(this).removeClass("btn-actived");
             $(this).children().removeClass("h-iconls-color-white");
             if($(this).children().hasClass("linear-icon-camera-crossed")){
                 $(this).children().removeClass("linear-icon-camera-crossed");
@@ -17,6 +18,7 @@ $(document).ready(function () {
            }
         }
         else {
+            $(this).addClass("btn-actived");
             $(this).children().addClass("h-iconls-color-white");
             if($(this).children().hasClass("linear-icon-camera")){
                 $(this).children().removeClass("linear-icon-camera");
@@ -31,6 +33,60 @@ $(document).ready(function () {
         }
         var attr = $(this).attr
     });
+
+
+    // trick to prevent scrolling when opening/closing button
+    function noScrollFn() {
+      window.scrollTo(scrollPosition ? scrollPosition.x : 0, scrollPosition ? scrollPosition.y : 0);
+    }
+    function noScroll() {
+      window.removeEventListener('scroll', scrollHandler);
+      window.addEventListener('scroll', noScrollFn);
+    }
+    function scrollFn() {
+      window.addEventListener('scroll', scrollHandler);
+    }
+    function canScroll() {
+      window.removeEventListener('scroll', noScrollFn);
+      scrollFn();
+    }
+    function scrollHandler() {
+      if (!didScroll) {
+        didScroll = true;
+        setTimeout(function () { scrollPage(); }, 60);
+      }
+    };
+    function scrollPage() {
+      scrollPosition = { x: window.pageXOffset || docElem.scrollLeft, y: window.pageYOffset || docElem.scrollTop };
+      didScroll = false;
+    };
+    scrollFn();
+
+    [].slice.call(document.querySelectorAll('.morph-button')).forEach(function (bttn) {
+      new UIMorphingButton(bttn, {
+        closeEl: '.linear-icon-cross2',
+        onBeforeOpen: function () {
+           // don't allow to scroll
+              noScroll();
+            },
+            onAfterOpen: function () {
+              // can scroll again
+              canScroll();
+            },
+            onBeforeClose: function () {
+              // don't allow to scroll
+              noScroll();
+            },
+            onAfterClose: function () {
+              // can scroll again
+              canScroll();
+            }
+          });
+        });
+        // for demo purposes only
+        [].slice.call(document.querySelectorAll('.actbtn')).forEach(function (bttn) {
+          bttn.addEventListener('click', function (ev) { ev.preventDefault(); });
+        });
 
 });
 
