da12eb7fec02e2ac3157f4134318ac99761b91e9
INFLEXION\Harish123 <harish.r@inflexiontech.com>
2017-10-05 12:17:01 +0530

    updated spacelist.

--- scripts/spaces/spacelist.js
+++ scripts/spaces/spacelist.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  
+    $.getJSON('/jsonFilesSample/spacesJson.json', function(response) {
+        console.log(response);
+        var source = $('#cardId').html();
+        var template = Handlebars.compile(source);
+        var result = template(response);
+        $('#List').html(result);
+    });
 });
