1f86687ebb94eb3645b59df1fdb583115239ff86
INFLEXION\Harish <harish.r@inflexiontech.com>
2017-10-10 15:45:18 +0530

    Bind the data for API response and using Handlebars for spacelist page.

--- scripts/spaces/spacelist.js
+++ scripts/spaces/spacelist.js
@@ -1,47 +1,28 @@
 $(document).ready(function () {
     var offset = 0, pages;
+    //GET coSpaces records
     httpGet(apiType.GET_COSPACES + "?offset="+offset+"&limit="+queryTypes.LIMIT, function (resp) {
-        var totalRec = resp.data.total;
-        pages = Math.ceil((parseInt(resp.data.total)/queryTypes.LIMIT) + 1);
+        var totalRec = resp.data.total; //Total coSpace records
+        pages = Math.ceil((parseInt(resp.data.total)/queryTypes.LIMIT) + 1); //No of pages in pagination
+        //Pagination Logic
         $('.sync-pagination').twbsPagination({
-            totalPages:pages,
+            totalPages: pages,
             hideOnlyOnePage: true,
             onPageClick: function(evnt, page){
-                debugger;
                 offset = queryTypes.LIMIT * (page - 1);
+                //If offset is greater than total records
                 if(offset > totalRec)
                     offset = totalRec - (totalRec - (queryTypes.LIMIT * (page - 2)));
+                //If offset is lesser than total records
                 httpGet(apiType.GET_COSPACES + "?offset="+offset+"&limit="+queryTypes.LIMIT, function (resp) {
-            
-                })
+                    //Bind the response using Handlebars
+                    var coSpacelist = resp.data;
+                    var source = $('#cardId').html();
+                    var template = Handlebars.compile(source);
+                    $('#List').html( template(coSpacelist));
+                  
+                });
             }
-        })
-    })
-
-    // for (var i = 0; i <= totalRecords;) {
-    //     httpGet(apiType.GET_COSPACES + "?offset=" + i + "&limit=8", function (resp) {
-    //         debugger;
-    //         var records = resp.data.total;
-    //         localStorage.setItem("recordCount", records);
-    //         // console.log("Total Pages: ", records / 8);
-    //         var pages = Math.ceil(records / 8);
-    //         // console.log("Pages: ", pages);
-    //         //Pagination logic
-    //         $('.sync-pagination').twbsPagination({
-    //             totalPages: pages,
-    //             hideOnlyOnePage: true,
-    //             onPageClick: function (evt, page) {
-    //                 debugger;
-    //                 var coSpacelist = resp.data.coSpaces;
-    //                 var source = $('#cardId').html();
-    //                 var template = Handlebars.compile(source);
-    //                 $('#List').html( template(coSpacelist));
-    //                 console.log(coSpacelist);
-    //             }
-    //         });
-    //     });
-    //     totalRecords = localStorage.getItem("recordCount");
-    //     console.log(totalRecords);
-    //     i=i+8;
-    // }
+        });
+    });
 });
