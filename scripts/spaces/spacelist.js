f4d262cc99defe9cb709907d21b67aaf12b86bf7
INFLEXION\sreeharsha.gv <sreeharsha.gv@inflexiontech.com>
2017-10-11 15:21:48 +0530

    Working on Filter cospaces ..!

--- scripts/spaces/spacelist.js
+++ scripts/spaces/spacelist.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     var offset = 0, pages;
     //GET coSpaces records
     httpGet(apiType.GET_COSPACES + "?offset="+offset+"&limit="+queryTypes.LIMIT, function (resp) {
@@ -26,5 +27,17 @@ $(document).ready(function () {
         });
     });
 
-    
+    // BEGIN SEARCH FILTER
+        $('#filter').keyup(function(){
+            debugger;
+            var input = $('#filter').val();
+            httpGet(apiType.GET_COSPACES + "?filter=" + input, function(resp){
+                console.log(resp);
+                var coSpacelist = resp.data;
+                var source = $('#cardId').html();
+                var template = Handlebars.compile(source);
+                $('#List').html( template(coSpacelist));
+            });  
+        });
+    // END SEARCH FILTER 
 });
