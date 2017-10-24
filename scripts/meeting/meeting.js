0d0ea701a9085b9b010fc19ca050cce6163c3f08
INFLEXION\Harish <harish.r@inflexiontech.com>
2017-10-24 14:16:37 +0530

    resolve conflicts in site.js

--- scripts/meeting/meeting.js
+++ scripts/meeting/meeting.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-    
+    // BEGIN NEW MEETING FORM VALIDATION
+    $('.done-btn').click(function(){
+        debugger;
+        if($('form').hasClass('validate-form')){
+            var resultItem = [];
+            $('.validate-text').each(function(i, obj){
+                resultItem.push(validateText(obj));
+            });
+        }
+    });
+    // END NEW MEETING FORM VALIDATION
 });
